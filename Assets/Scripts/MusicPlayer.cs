71e799db523732bde9c3080fc87a0a7401e9164c
Robert Hutchinson <Robhutchdev616@gmail.com>
2017-05-02 15:40:33 +0100

    Added ball

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private static MusicPlayer music;
+
+    private void Awake() {
+        Debug.Log(" Id is =" + this.GetInstanceID().ToString());
+    }
+
+    public void getInstance() {
+        if(music != null) {
+            Destroy(gameObject);
+            Debug.Log("this");
+        }else {
+            music = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+       
+    }
+	// Use this for initialization
+	void Start () {
+        Debug.Log(" Id is =" + this.GetInstanceID().ToString());
+        getInstance();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
